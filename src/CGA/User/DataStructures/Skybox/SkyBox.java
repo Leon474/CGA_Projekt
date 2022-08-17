c066c99e8b7964498c0c148df31e0e561cb47505
Leon474 <leon.pratsch@smail.th-koeln.de>
2022-08-17 20:12:03 +0200

    - skybox adjustments

--- src/CGA/User/DataStructures/Skybox/SkyBox.java
+++ src/CGA/User/DataStructures/Skybox/SkyBox.java
@@ -5,14 +5,16 @@ import CGA.User.DataStructures.ShaderProgram;
 import CGA.User.DataStructures.Texture2D;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.openvr.Texture;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
-import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
-import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.opengl.GL13.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL15.glDeleteBuffers;
 import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
@@ -20,6 +22,8 @@ import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.opengles.EXTSparseTexture.GL_TEXTURE_CUBE_MAP;
 import static org.lwjgl.opengles.GLES30.GL_TEXTURE_WRAP_R;
+import static org.lwjgl.stb.STBImage.*;
+import static org.lwjgl.stb.STBImage.stbi_image_free;
 
 
 public class SkyBox extends Transformable {
@@ -33,6 +37,7 @@ public class SkyBox extends Transformable {
     int skyboxEBO = 1;
     private int count;
     private Texture2D cubemapTexture;
+    private int texID;
 
 
     public SkyBox(float[] vertexdata, int[] indexdata, VertexAttribute[] attributes) throws Exception {
@@ -80,22 +85,62 @@ public class SkyBox extends Transformable {
     }
 
 
+    public int loadCubemap(String facesCubemap[]) {
+
+        texID = glGenTextures();                        // von mir
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texID);      // von mir  (bei texid kommt cubemapTexture????
+
+        for (int i = 0; i < facesCubemap.length; i++) {
+            System.out.println("load map aufruf"+i);
+            IntBuffer width = BufferUtils.createIntBuffer(1);
+            IntBuffer height = BufferUtils.createIntBuffer(1);
+            IntBuffer nrChannels = BufferUtils.createIntBuffer(1);
+
+            ByteBuffer imageData = stbi_load(facesCubemap[i], width, height, nrChannels, 0);
+
+            if (imageData != null) {
+
+                stbi_set_flip_vertically_on_load(true); // vllt auch true
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width.get(), height.get(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+                stbi_image_free(imageData);
+
+                //unbind(); // von mir hinzugefügt
+            } else {
+                System.out.println("Failed to load Texture: "+ facesCubemap[i]);
+                stbi_image_free(imageData);
+            }
+        }
+
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+        return texID;
+    }
+
+
     /**
      * renders the mesh
      */
-    public void render() {
+    public void render(ShaderProgram shaderProgram, int cubemapTexture) {
         //TODO: Call the rendering method every frame
+        //glDepthFunc(GL_LEQUAL);
         glBindVertexArray(skyboxVAO);
         glActiveTexture(GL_TEXTURE0);
-        //glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
-        glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, 0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        //glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36); // im anderen tutorial
         glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // im anderen tutorial
     }
 
-    public void render(ShaderProgram shaderProgram) {
+    /*public void render(ShaderProgram shaderProgram) {
         //material.bind(shaderProgram);
         render();
-    }
+    }*/
 
     /**
      * Deletes the previously allocated OpenGL objects for this mesh
--- src/CGA/User/DataStructures/TextureSkybox.java
+++ src/CGA/User/DataStructures/TextureSkybox.java
@@ -1,13 +1,15 @@
 package CGA.User.DataStructures;
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL13;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
-import static org.lwjgl.opengl.GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X;
+import static org.lwjgl.opengl.GL13.*;
+import static org.lwjgl.opengl.GL30.glGenerateMipmap;
 import static org.lwjgl.opengles.EXTSparseTexture.GL_TEXTURE_CUBE_MAP;
 import static org.lwjgl.opengles.GLES30.GL_TEXTURE_WRAP_R;
 import static org.lwjgl.stb.STBImage.*;
@@ -17,7 +19,12 @@ public class TextureSkybox {
 
     private int texID;
 
-    public TextureSkybox(String facesCubemap[]) {
+    /*public TextureSkybox(String facesCubemap[]) {
+
+
+        texID = glGenTextures();                        // von mir
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texID);      // von mir
+
 
         for (int i = 0; i < facesCubemap.length; i++) {
             IntBuffer width = BufferUtils.createIntBuffer(1);
@@ -27,15 +34,63 @@ public class TextureSkybox {
             ByteBuffer imageData = stbi_load(facesCubemap[i], width, height, nrChannels, 0);
 
             if (imageData != null) {
+
                 stbi_set_flip_vertically_on_load(true); // vllt auch true
                 glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width.get(), height.get(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
                 stbi_image_free(imageData);
+
+                //unbind(); // von mir hinzugefügt
             } else {
                 System.out.println("Failed to load Texture: "+ facesCubemap[i]);
                 stbi_image_free(imageData);
             }
         }
+
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    }*/
+
+    public int loadCubemap(String facesCubemap[]) {
+
+        texID = glGenTextures();                        // von mir
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texID);      // von mir
+
+        for (int i = 0; i < facesCubemap.length; i++) {
+            IntBuffer width = BufferUtils.createIntBuffer(1);
+            IntBuffer height = BufferUtils.createIntBuffer(1);
+            IntBuffer nrChannels = BufferUtils.createIntBuffer(1);
+
+            ByteBuffer imageData = stbi_load(facesCubemap[i], width, height, nrChannels, 0);
+
+            if (imageData != null) {
+
+                stbi_set_flip_vertically_on_load(true); // vllt auch true
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width.get(), height.get(), 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+                stbi_image_free(imageData);
+
+                //unbind(); // von mir hinzugefügt
+            } else {
+                System.out.println("Failed to load Texture: "+ facesCubemap[i]);
+                stbi_image_free(imageData);
+            }
+        }
+
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+        return texID;
     }
+
+
+
 
     /*public void skyBoxTexture(String facesCubemap[]){
         for (int i = 0; i < 6; i++) {
@@ -69,9 +124,17 @@ public class TextureSkybox {
         unbind();
     }
 
+    /*public void bind(int textureUnit) {
+        //TODO: Place your code here
+        glActiveTexture(GL_TEXTURE0 + textureUnit);
+        glBindTexture(GL_TEXTURE_2D, texID);
+    }*/
+
     public void unbind() {
         //TODO: Place your code here
-        glBindTexture(GL_TEXTURE_2D,0);
+        //glBindTexture(GL_TEXTURE_2D,0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP,0);
+
     }
 
     public void cleanup() {
